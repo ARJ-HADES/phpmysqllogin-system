34c41b909f8d27cb7c63b8008e78b052402c1c05
ARJ-HADES <68515819+ARJ-HADES@users.noreply.github.com>
2021-03-30 10:45:37 +0400

    final

---
+++ send_sms.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$basic  = new \Vonage\Client\Credentials\Basic(c6863c25, VONAGE_API_SECRET);
+$client = new \Vonage\Client($basic);
+
+$response = $client->sms()->send(
+    new \Vonage\SMS\Message\SMS(TO_NUMBER, BRAND_NAME, 'A text message sent using the Nexmo SMS API')
+);
+
+$message = $response->current();
+
+if ($message->getStatus() == 0) {
+    echo "The message was sent successfully\n";
+} else {
+    echo "The message failed with status: " . $message->getStatus() . "\n";
+}
+?>
---
+++ sms.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sending SMS </title>
+
+</head>
+<body>
+
+	<form method = "post">
+		<label>MOBILE NUMBER</label>
+		<input type ="text" name="num">
+		<br><br>
+		<label>Country code</label>
+		<select name ="code">
+				<option value ="">Select here....</option>
+				<option value ="91">India - +91</option>
+				<option value = "971">UAE - +971</option>
+		</select>
+		<br><br
+		<label> Enter the message</label>
+		<input type = "text" name ="message">
+		<input type="submit" name="submit">
+
+	</form>
+
+
+
+
+
+</body>
+</html>
